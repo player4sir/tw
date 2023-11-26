c408e63ae9c1ee4e06793168164cac3ce8bc276f
我是海军呀 <31569037+player4sir@users.noreply.github.com>
2023-11-26 21:34:00 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+# 导入所需的模块
 from flask import Flask, request, jsonify
 import yt_dlp
 
@@ -12,29 +13,25 @@ def video():
     # 如果没有提供网址，返回一个错误信息
     if not url:
         return jsonify({'error': 'No url provided'})
-
+    # 创建一个 yt-dlp 下载器对象，使用 --cookies-from-browser 参数让 yt-dlp 从你的浏览器获取 cookie
+    ydl = yt_dlp.YoutubeDL({'cookies-from-browser': 'chrome'})
     # 尝试提取视频信息
     try:
-        # 创建一个 yt-dlp 下载器对象
-        # 注意: 使用明文密码不安全，建议使用更安全的认证方法，如 OAuth 或 API 密钥
-        ydl = yt_dlp.YoutubeDL({})
         
-        info = ydl.extract_info(url, download=False)       
-        # 过滤掉包含"hls"的格式
-        filtered_formats = [f for f in info['formats'] if 'hls' not in f['format']]
-
-        # 返回一个 JSON 格式的响应，包含视频的元数据和下载链接
-        return jsonify({
-            'formats': [{'format': f['format'], 'url': f['url']} for f in filtered_formats]
-        })
-
+        
+        info = ydl.extract_info(url, download=False)
     except yt_dlp.utils.DownloadError as e:
-        # 如果出现其他下载错误，返回一个错误信息
+        # 如果出现错误，返回一个错误信息
         return jsonify({'error': str(e)})
+    # 返回一个 JSON 格式的响应，包含视频的元数据和下载链接
+    return jsonify({
+        'title': info['title'],
+        # 'uploader': info['uploader'],
+        'duration': info['duration'],
+        # 修改这一行，使用列表推导式，只保留格式为 mp4 的 url
+        'formats': [{'format': f['format'], 'url': f['url']} for f in info['formats'] if f['ext'] == 'mp4']
+    })
 
 # 运行应用
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=False)
-
-
-
+    app.run(debug=False)
